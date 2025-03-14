f5a836bece32850152b295b48fe3f32567aa317c
ryan.d.varela@gmail.com <ryan.d.varela@gmail.com>
2025-03-14 15:42:35 -0400

    moving css for faster running.

--- src/App.vue
+++ src/App.vue
@@ -89,4 +89,333 @@ button {
 button:hover {
   background-color: #f1f1f1;
 }
+
+  .adminpaneldashboard {
+    background-image: url('../src/assets/Admin-Panel-Dashboard.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .aigenretransformation {
+    background-image: url('../src/assets/AI-Genre-Transformation.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .aipoweredsmartcontractauditingsecurity {
+    background-image: url('../src/assets/AI-Powered-Smart-Contract-Auditing-Security.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .bigscreenandroidappoverview {
+    background-image: url('../src/assets/Big-Screen-Android-App-Overview.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .bigscreenhome {
+    background-image: url('../src/assets/Big-Screen-Home.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .bigscreenhomescreen {
+    background-image: url('../src/assets/Big-Screen-Home.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .BigScreenTokenNativeCryptocurrency {
+    background-image: url('../src/assets/Big-Screen-Token-Native-Cryptocurrency.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .BlockchainBasedContentLicensingAntiPiracy {
+    background-image: url('../src/assets/Blockchain-Based-Content-Licensing-Anti-Piracy.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .BSTTokenStakingRewards {
+    background-image: url('../src/assets/BST-Token-Staking-Rewards.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .CommunitySocialFeatures {
+    background-image: url('../src/assets/Community-Social-Features.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .CreatorDashboard {
+    background-image: url('../src/assets/Creator-Dashboard.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .CrossChainCompatibilityOverview {
+    background-image: url('../src/assets/Cross-Chain-Compatibility-Overview.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .DecentralizedAdNetworkTokenizedSponsorships {
+    background-image: url('../src/assets/Decentralized-Ad-Network-Tokenized-Sponsorships.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .DecentralizedFilmFunding {
+    background-image: url('../src/assets/Decentralized-Film-Funding.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .DecentralizedGovernanceDAOVotingSystem {
+    background-image: url('../src/assets/Decentralized-Governance-DAO-Voting-System.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .Discover {
+    background-image: url('../src/assets/Discover.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .DynamicGiftingSystem {
+    background-image: url('../src/assets/Dynamic-Gifting-System.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .DynamicRevenueSharingforContentCreators {
+    background-image: url('../src/assets/Dynamic-Revenue-Sharing-for-Content-Creators.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .FanEngagementInteractiveContent {
+    background-image: url('../src/assets/Fan-Engagement-Interactive-Content.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .InteractiveStorytelling {
+    background-image: url('../src/assets/Interactive-Storytelling.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .LandingHome {
+    background-image: url('../src/assets/Landing-Home.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .Layer2ScalingZeroKnowledgeProofs {
+    background-image: url('../src/assets/Layer-2-Scaling-Zero-Knowledge-Proofs.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .LiveStreamingHub {
+    background-image: url('../src/assets/Live-Streaming-Hub.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .MediaPlayer {
+    background-image: url('../src/assets/Media-Player.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .MetaverseCinemas {
+    background-image: url('../src/assets/Metaverse-Cinemas.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .MonetizationWeb3IntegrationPages {
+    background-image: url('../src/assets/Monetization-Web3-Integration-Pages.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .MonetizationWeb3Tokenization {
+    background-image: url('../src/assets/Monetization-Web3-Tokenization.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .MovieTVShowDetailPage {
+    background-image: url('../src/assets/Movie-TV-Show-Detail-Page.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .MovieTVShowDetailStreamingHub {
+    background-image: url('../src/assets/Movie-TV-Show-Detail-Streaming-Hub.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .NFTBasedMovieOwnershipCollectibles {
+    background-image: url('../src/assets/NFT-Based-Movie-Ownership-Collectibles.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .NFTBasedTicketingExclusiveEventAccess {
+    background-image: url('../src/assets/NFT-Based-Ticketing-Exclusive-Event-Access.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .NonprofitImpactDashboard {
+    background-image: url('../src/assets/Nonprofit-Impact-Dashboard.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .ParentalControlDashboard {
+    background-image: url('../src/assets/Parental-Control-Dashboard.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .ProfileHome {
+    background-image: url('../src/assets/Profile-Home.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .SearchHome {
+    background-image: url('../src/assets/Search-Home.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .SecurityandPrivacyPages {
+    background-image: url('../src/assets/Security-and-Privacy-Pages.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .SecurityModerationPrivacyDashboard {
+    background-image: url('../src/assets/Security-Moderation-Privacy-Dashboard.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .SecurityPrivacySettings {
+    background-image: url('../src/assets/Security-Privacy-Settings.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .SmartTVMobileAppInterface {
+    background-image: url('../src/assets/Smart-TV-Mobile-App-Interface.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .SocialCommunityPages {
+    background-image: url('../src/assets/Social-Community-Pages.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .SocialImpactNonprofitInitiatives {
+    background-image: url('../src/assets/Social-Impact-Nonprofit-Initiatives.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .SocialMediaSharingViralContentDistribution {
+    background-image: url('../src/assets/Social-Media-Sharing-Viral-Content-Distribution.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .UserFacingPages {
+    background-image: url('../src/assets/User-Facing-Pages.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .UserProfileWatchHistory {
+    background-image: url('../src/assets/User-Profile-Watch-History.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .WatchPage {
+    background-image: url('../src/assets/Watch-Page.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .WatchtoEarnRewards {
+    background-image: url('../src/assets/Watch-to-Earn-Rewards.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
+
+  .Web3MarketplaceforDigitalMovieCollectibles {
+    background-image: url('../src/assets/Web3-Marketplace-for-Digital-Movie-Collectibles.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+  }
 </style>
--- src/views/AIPoweredSmartContractAuditingSecurity.vue
+++ src/views/AIPoweredSmartContractAuditingSecurity.vue
@@ -10,10 +10,5 @@
   </script>
   
   <style scoped>
-  .aipoweredsmartcontractauditingsecurity {
-    background-image: url('../assets/AI-Powered-Smart-Contract-Auditing-Security.svg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    height: 100%;
-  }
+
   </style>
