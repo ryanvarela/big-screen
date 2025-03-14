310c7217b305260befaddf6f41acd228418a0248
ryan.d.varela@gmail.com <ryan.d.varela@gmail.com>
2025-03-14 15:25:53 -0400

    final update

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,56 @@
 <template>
   <div style="height: 100%; background-color: black; display: inline;">
-    <div style="width: 100px; height: 100%; display: inline-block; background-color: black; position: absolute; bottom: 0px;">
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/AI-Genre-Tranformation" style="color: white;">AI-Genre-Tranformation</router-link><br>
-      <router-link to="/AI-Powered-Smart-Contract-Auditing-&-Security" style="color: white;">AI-Powered-Smart-Contract-Auditing-&-Security</router-link><br>
-      <router-link to="/Big-Screen-Android-App-Overview" style="color: white;">Big-Screen-Android-App-Overview</router-link><br>
-      <router-link to="/Big-Screen-Home" style="color: white;">Big-Screen-Home</router-link><br>
-      <router-link to="/Big-Screen-Home-Screen" style="color: white;">Big-Screen-Home-Screen</router-link><br>
-      <router-link to="/Big-Screen-Token-Native-Cryptocurrency" style="color: white;">Big-Screen-Token-Native-Cryptocurrency</router-link><br>
-      <router-link to="/Blockchain-Based-Content-Licensing-Anti-Piracy" style="color: white;">Blockchain-Based-Content-Licensing-Anti-Piracy</router-link><br>
-      <router-link to="/BST-Token-Staking-Rewards" style="color: white;">BST-Token-Staking-Rewards</router-link><br>
-      <router-link to="/Community-Social-Features" style="color: white;">Community-Social-Features</router-link><br>
-      <router-link to="/Creator-Dashboard" style="color: white;">Creator-Dashboard</router-link><br>
-      <router-link to="/Creator-Monetization-Pages" style="color: white;">Creator-Monetization-Pages</router-link><br>
-      <router-link to="/Cross-Chain-Compatibility-Overview" style="color: white;">Cross-Chain-Compatibility-Overview</router-link><br>
-      <router-link to="/Decentralized-Ad-Network-Tokenized-Sponsorships" style="color: white;">Decentralized-Ad-Network-Tokenized-Sponsorships</router-link><br>
-      <router-link to="/Decentralized-Film-Funding" style="color: white;">Decentralized-Film-Funding</router-link><br>
-      <router-link to="/Decentralized-Governance-DAO-Voting-System" style="color: white;">Decentralized-Governance-DAO-Voting-System</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
-      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+    <div style="width: 200px; height: 100%; display: inline-block; background-color: black; position: absolute; overflow-y: auto; bottom: 0px;">
+      <router-link to="/Admin-Panel-Dashboard" style="color: white; text-decoration: none;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/AI-Genre-Tranformation" style="color: white; text-decoration: none;">AI-Genre-Tranformation</router-link><br>
+      <router-link to="/AI-Powered-Smart-Contract-Auditing-&-Security" style="color: white; text-decoration: none;">AI-Powered-Smart-Contract-Auditing-&-Security</router-link><br>
+      <router-link to="/Big-Screen-Android-App-Overview" style="color: white; text-decoration: none;">Big-Screen-Android-App-Overview</router-link><br>
+      <router-link to="/Big-Screen-Home" style="color: white; text-decoration: none;">Big-Screen-Home</router-link><br>
+      <router-link to="/Big-Screen-Home-Screen" style="color: white; text-decoration: none;">Big-Screen-Home-Screen</router-link><br>
+      <router-link to="/Big-Screen-Token-Native-Cryptocurrency" style="color: white; text-decoration: none;">Big-Screen-Token-Native-Cryptocurrency</router-link><br>
+      <router-link to="/Blockchain-Based-Content-Licensing-Anti-Piracy" style="color: white; text-decoration: none;">Blockchain-Based-Content-Licensing-Anti-Piracy</router-link><br>
+      <router-link to="/BST-Token-Staking-Rewards" style="color: white; text-decoration: none;">BST-Token-Staking-Rewards</router-link><br>
+      <router-link to="/Community-Social-Features" style="color: white; text-decoration: none;">Community-Social-Features</router-link><br>
+      <router-link to="/Creator-Dashboard" style="color: white; text-decoration: none;">Creator-Dashboard</router-link><br>
+      <router-link to="/Creator-Monetization-Pages" style="color: white; text-decoration: none;">Creator-Monetization-Pages</router-link><br>
+      <router-link to="/Cross-Chain-Compatibility-Overview" style="color: white; text-decoration: none;">Cross-Chain-Compatibility-Overview</router-link><br>
+      <router-link to="/Decentralized-Ad-Network-Tokenized-Sponsorships" style="color: white; text-decoration: none;">Decentralized-Ad-Network-Tokenized-Sponsorships</router-link><br>
+      <router-link to="/Decentralized-Film-Funding" style="color: white; text-decoration: none;">Decentralized-Film-Funding</router-link><br>
+      <router-link to="/Decentralized-Governance-DAO-Voting-System" style="color: white; text-decoration: none;">Decentralized-Governance-DAO-Voting-System</router-link><br>
+      <router-link to="/Discover" style="color: white; text-decoration: none;">Discover</router-link><br>
+      <router-link to="/Dynamic-Gifting-System" style="color: white; text-decoration: none;">Dynamic-Gifting-System</router-link><br>
+      <router-link to="/Dynamic-Revenue-Sharing-for-Content-Creators" style="color: white; text-decoration: none;">Dynamic-Revenue-Sharing-for-Content-Creators</router-link><br>
+      <router-link to="/Fan-Engagement-Interactive-Content" style="color: white; text-decoration: none;">Fan-Engagement-Interactive-Content</router-link><br>
+      <router-link to="/Interactive-Storytelling" style="color: white; text-decoration: none;">Interactive-Storytelling</router-link><br>
+      <router-link to="/Landing-Home" style="color: white; text-decoration: none;">Landing-Home</router-link><br>
+      <router-link to="/Layer-2-Scaling-Zero-Knowledge-Proofs" style="color: white; text-decoration: none;">Layer-2-Scaling-Zero-Knowledge-Proofs</router-link><br>
+      <router-link to="/Live-Streaming-Hub" style="color: white; text-decoration: none;">Live-Streaming-Hub</router-link><br>
+      <router-link to="/Media-Player" style="color: white; text-decoration: none;">Media-Player</router-link><br>
+      <router-link to="/Metaverse-Cinemas" style="color: white; text-decoration: none;">Metaverse-Cinemas</router-link><br>
+      <router-link to="/Monetization-Web3-Integration-Pages" style="color: white; text-decoration: none;">Monetization-Web3-Integration-Pages</router-link><br>
+      <router-link to="/Monetization-Web3-Tokenization" style="color: white; text-decoration: none;">Monetization-Web3-Tokenization</router-link><br>
+      <router-link to="/Movie-TV-Show-Detail-Page" style="color: white; text-decoration: none;">Movie-TV-Show-Detail-Page</router-link><br>
+      <router-link to="/Movie-TV-Show-Detail-Streaming-Hub" style="color: white; text-decoration: none;">Movie-TV-Show-Detail-Streaming-Hub</router-link><br>
+      <router-link to="/NFT-Based-Movie-Ownership-Collectibles" style="color: white; text-decoration: none;">NFT-Based-Movie-Ownership-Collectibles</router-link><br>
+      <router-link to="/NFT-Based-Ticketing-Exclusive-Event-Access" style="color: white; text-decoration: none;">NFT-Based-Ticketing-Exclusive-Event-Access</router-link><br>
+      <router-link to="/Nonprofit-Impact-Dashboard" style="color: white; text-decoration: none;">Nonprofit-Impact-Dashboard</router-link><br>
+      <router-link to="/Parental-Control-Dashboard" style="color: white; text-decoration: none;">Parental-Control-Dashboard</router-link><br>
+      <router-link to="/Profile-Home" style="color: white; text-decoration: none;">Profile-Home</router-link><br>
+      <router-link to="/Search-Home" style="color: white; text-decoration: none;">Search-Home</router-link><br>
+      <router-link to="/Security-and-Privacy-Pages" style="color: white; text-decoration: none;">Security-and-Privacy-Pages</router-link><br>
+      <router-link to="/Security-Moderation-Privacy-Dashboard" style="color: white; text-decoration: none;">Security-Moderation-Privacy-Dashboard</router-link><br>
+      <router-link to="/Security-Privacy-Settings" style="color: white; text-decoration: none;">Security-Privacy-Settings</router-link><br>
+      <router-link to="/Smart-TV-Mobile-App-Interface" style="color: white; text-decoration: none;">Smart-TV-Mobile-App-Interface</router-link><br>
+      <router-link to="/Social-Community-Pages" style="color: white; text-decoration: none;">Social-Community-Pages</router-link><br>
+      <router-link to="/Social-Impact-Nonprofit-Initiatives" style="color: white; text-decoration: none;">Social-Impact-Nonprofit-Initiatives</router-link><br>
+      <router-link to="/Social-Media-Sharing-Viral-Content-Distribution" style="color: white; text-decoration: none;">Social-Media-Sharing-Viral-Content-Distribution</router-link><br>
+      <router-link to="/User-Facing-Pages" style="color: white; text-decoration: none;">User-Facing-Pages</router-link><br>
+      <router-link to="/User-Profile-Watch-History" style="color: white; text-decoration: none;">User-Profile-Watch-History</router-link><br>
+      <router-link to="/Watch-Page" style="color: white; text-decoration: none;">Watch-Page</router-link><br>
+      <router-link to="/Watch-to-Earn-Rewards" style="color: white; text-decoration: none;">Watch-to-Earn-Rewards</router-link><br>
+      <router-link to="/Web3-Marketplace-for-Digital-Movie-Collectibles" style="color: white; text-decoration: none;">Web3-Marketplace-for-Digital-Movie-Collectibles</router-link><br>
     </div>
-    <div style="height: 100%;width: calc(100% - 110px);display: inline-block; position: relative; left: 100px;">
+    <div style="height: 100%;width: calc(100% - 200px);display: inline-block; position: relative; left: 200px;">
       <router-view></router-view>
     </div>
   </div>
@@ -72,7 +72,7 @@ body {
 
 .dark-theme {
   background-color: #121212;
-  color: white;
+  color: white; text-decoration: none;
 }
 
 button {
