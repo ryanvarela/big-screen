b3aebf35e1629e136c6bc083cbd1492831d85ec8
ryan.d.varela@gmail.com <ryan.d.varela@gmail.com>
2025-03-14 15:05:02 -0400

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,58 @@
 <template>
-  <div style="height: 100%;">
-    <router-view></router-view>
+  <div style="height: 100%; background-color: black; display: inline;">
+    <div style="width: 100px; height: 100%; display: inline-block; background-color: black; position: absolute; bottom: 0px;">
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/AI-Genre-Tranformation" style="color: white;">AI-Genre-Tranformation</router-link><br>
+      <router-link to="/AI-Powered-Smart-Contract-Auditing-&-Security" style="color: white;">AI-Powered-Smart-Contract-Auditing-&-Security</router-link><br>
+      <router-link to="/Big-Screen-Android-App-Overview" style="color: white;">Big-Screen-Android-App-Overview</router-link><br>
+      <router-link to="/Big-Screen-Home" style="color: white;">Big-Screen-Home</router-link><br>
+      <router-link to="/Big-Screen-Home-Screen" style="color: white;">Big-Screen-Home-Screen</router-link><br>
+      <router-link to="/Big-Screen-Token-Native-Cryptocurrency" style="color: white;">Big-Screen-Token-Native-Cryptocurrency</router-link><br>
+      <router-link to="/Blockchain-Based-Content-Licensing-Anti-Piracy" style="color: white;">Blockchain-Based-Content-Licensing-Anti-Piracy</router-link><br>
+      <router-link to="/BST-Token-Staking-Rewards" style="color: white;">BST-Token-Staking-Rewards</router-link><br>
+      <router-link to="/Community-Social-Features" style="color: white;">Community-Social-Features</router-link><br>
+      <router-link to="/Creator-Dashboard" style="color: white;">Creator-Dashboard</router-link><br>
+      <router-link to="/Creator-Monetization-Pages" style="color: white;">Creator-Monetization-Pages</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+      <router-link to="/Admin-Panel-Dashboard" style="color: white;">Admin Panel Dashboard</router-link><br>
+    </div>
+    <div style="height: 100%;width: calc(100% - 110px);display: inline-block; position: relative; left: 100px;">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
